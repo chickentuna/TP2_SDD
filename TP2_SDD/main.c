c8a0239ad16ced579c4301c8fc1581439c0c48b9
chickentuna <chickentuna@hotmail.com>
2012-04-27 19:42:33 +0200

    Hello world is functional. ---> Lets code !

--- TP2_SDD/main.c
+++ TP2_SDD/main.c
@@ -4,5 +4,12 @@
  *  Created on: 27 avr. 2012
  *      Author: Fanchon et Julien
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 
+int main(int argc, char** argv) {
+
+	printf("Hello world");
+	return EXIT_SUCCESS;
+}
