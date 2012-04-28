9726110ad0cd6a096a9a74760bb4f9ffdc3e892c
Ephasme <loup.peluso@gmail.com>
2012-04-28 11:43:41 +0200

    Modif mineures.

--- TP2_SDD/testlib/testlib.h
+++ TP2_SDD/testlib/testlib.h
@@ -20,44 +20,44 @@
 #undef ALLOC
 #define ALLOC(_n,_t) (_t*) malloc(_n*sizeof(_t))
 
-#define FOR_EACH(list, size, el, actions) {					\
-	int compteur;											\
-	if (list != NULL && size > 0) {							\
-		for (compteur = 0 ; compteur < size ; compteur++) {	\
-			typeof(list[0]) el = list[compteur];			\
-			actions											\
-		}													\
-	}														\
+#define FOR_EACH(list, size, el, actions) {							\
+	int compteur;													\
+	if (list != NULL && size > 0) {									\
+		for (compteur = 0 ; compteur < size ; compteur++) {			\
+			typeof(list[0]) el = list[compteur];					\
+			actions													\
+		}															\
+	}																\
 }
 
-#define creerTamponDonnee(donnee, type, texte) {	\
-	char* tampon = texte;							\
-	donnee = creerDonnee(type, tampon);				\
-	free(tampon);									\
+#define creerTamponDonnee(donnee, type, texte) {					\
+	char* tampon = texte;											\
+	donnee = creerDonnee(type, tampon);								\
+	free(tampon);													\
 }
 
 #define creerBoolDonnee(donnee, type, condition) {					\
 	creerTamponDonnee(donnee, type, (condition ? "VRAI" : "FAUX"));	\
 }
 
-#define TYPE_ENTREE "Entrée"
-#define TYPE_SORTIE "Sortie"
-#define TYPE_RETOUR "Retour"
+#define TYPE_ENTREE						"Entrée"
+#define TYPE_SORTIE						"Sortie"
+#define TYPE_RETOUR						"Retour"
 
-#define VRAI 1
-#define FAUX 0
+#define VRAI							1
+#define FAUX							0
 
-#define REUSSI 1
-#define ECHOUE 0
+#define REUSSI							1
+#define ECHOUE							0
 
-#define ASSERTION_LIST_MAX_SIZE 10
-#define DATA_LIST_MAX_SIZE 10
+#define ASSERTION_LIST_MAX_SIZE			20
+#define DATA_LIST_MAX_SIZE				20
 
 typedef unsigned char flag_t;
 
-#define FLAG_EMPTY					0
-#define FLAG_HIDE_ASSERTS			1
-#define FLAG_HIDE_DATAS				2
+#define FLAG_EMPTY						0
+#define FLAG_HIDE_ASSERTS				1
+#define FLAG_HIDE_DATAS					2
 
 /**************************************************************************************
  *          STRUCTURES & TYPES
